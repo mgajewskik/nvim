23138487af5cf278c307249dfdc89d2c57024132
mgajewskik <mgajewskik@gmail.com>
2021-10-17 18:21:43 +0200

    Add initial Arch configs

---
+++ lua/treesitter.lua
@@ -0,0 +1,13 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = false
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+  },
+}
