78ca9745e8483553091779cc29ff206a17ea8401
mgajewskik <mgajewskik@gmail.com>
2024-12-27 16:00:36 +0100

    Add DAP configuration for go delve

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,37 +1,54 @@
 return {
-   -- {
-   --    "mfussenegger/nvim-dap",
-   --    lazy = true,
-   -- },
-   -- {
-   --    "theHamsta/nvim-dap-virtual-text",
-   --    dependencies = {
-   --       "mfussenegger/nvim-dap",
-   --    },
-   --    lazy = true,
-   --    opts = {
-   --       commented = true,
-   --    },
-   -- },
-   -- {
-   --    "rcarriga/nvim-dap-ui",
-   --    dependencies = {
-   --       "mfussenegger/nvim-dap",
-   --    },
-   --    lazy = true,
-   -- },
-   -- {
-   --    "mfussenegger/nvim-dap-python",
-   --    dependencies = {
-   --       "mfussenegger/nvim-dap",
-   --    },
-   --    lazy = true,
-   -- },
-   -- {
-   --    "leoluz/nvim-dap-go",
-   --    dependencies = {
-   --       "mfussenegger/nvim-dap",
-   --    },
-   --    lazy = true,
-   -- },
+   {
+      "mfussenegger/nvim-dap",
+      dependencies = {
+         "leoluz/nvim-dap-go",
+         "rcarriga/nvim-dap-ui",
+         "theHamsta/nvim-dap-virtual-text",
+         "nvim-neotest/nvim-nio",
+         "williamboman/mason.nvim",
+      },
+      config = function()
+         local dap = require("dap")
+         local ui = require("dapui")
+
+         require("dapui").setup()
+         require("dap-go").setup()
+         require("nvim-dap-virtual-text").setup()
+
+         -- vim.fn.sign_define(
+         --    "DapBreakpoint",
+         --    { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+         -- )
+
+         vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
+         vim.keymap.set("n", "<space>dh", dap.run_to_cursor)
+
+         -- Eval var under cursor
+         vim.keymap.set("n", "<space>dv", function()
+            require("dapui").eval(nil, { enter = true })
+         end)
+
+         vim.keymap.set("n", "<leader>dc", dap.continue)
+         vim.keymap.set("n", "<leader>dj", dap.step_into)
+         vim.keymap.set("n", "<leader>dl", dap.step_over)
+         vim.keymap.set("n", "<leader>dk", dap.step_out)
+         -- not supported in Go
+         -- vim.keymap.set("n", "<F5>", dap.step_back)
+         vim.keymap.set("n", "<leader>dr", dap.restart)
+
+         dap.listeners.before.attach.dapui_config = function()
+            ui.open()
+         end
+         dap.listeners.before.launch.dapui_config = function()
+            ui.open()
+         end
+         dap.listeners.before.event_terminated.dapui_config = function()
+            ui.close()
+         end
+         dap.listeners.before.event_exited.dapui_config = function()
+            ui.close()
+         end
+      end,
+   },
 }
