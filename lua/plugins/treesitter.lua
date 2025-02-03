905bbf99642980aa3643e13a0ab4c7afff55a974
mgajewskik <mgajewskik@gmail.com>
2025-02-03 08:08:09 +0100

    Add contextindent config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -119,4 +119,11 @@ return {
          vim.treesitter.language.register("markdown", "octo")
       end,
    },
+   {
+      "wurli/contextindent.nvim",
+      -- This is the only config option; you can use it to restrict the files
+      -- which this plugin will affect (see :help autocommand-pattern).
+      opts = { pattern = "*" },
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+   },
 }
