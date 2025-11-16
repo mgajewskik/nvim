5d69cdf79ffeef1e561c26b8d6859a7d646c8e61
mgajewskik <mgajewskik@gmail.com>
2025-11-16 13:02:34 +0100

    Cleanup legacy nvim-cmp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
       dependencies = {
          "mason.nvim",
          { "mason-org/mason-lspconfig.nvim", config = function() end },
-         -- "hrsh7th/cmp-nvim-lsp",
          "saghen/blink.cmp",
       },
       event = { "BufReadPre", "BufNewFile" },
@@ -286,105 +285,4 @@ return {
          },
       },
    },
-   -- {
-   --    "hrsh7th/nvim-cmp",
-   --    dependencies = {
-   --       "neovim/nvim-lspconfig",
-   --       "onsails/lspkind-nvim",
-   --       "hrsh7th/cmp-buffer",
-   --       "hrsh7th/cmp-nvim-lsp",
-   --       "hrsh7th/cmp-path",
-   --       "hrsh7th/cmp-cmdline",
-   --       "lukas-reineke/cmp-rg",
-   --       "hrsh7th/cmp-nvim-lsp-signature-help",
-   --       "hrsh7th/cmp-nvim-lsp-document-symbol",
-   --       -- "L3MON4D3/LuaSnip",
-   --       "hrsh7th/cmp-nvim-lua",
-   --       "petertriho/cmp-git",
-   --    },
-   --    config = function()
-   --       local cmp = require("cmp")
-   --       local lspkind = require("lspkind")
-   --
-   --       ---@diagnostic disable-next-line: missing-fields
-   --       cmp.setup({
-   --          ---@diagnostic disable-next-line: missing-fields
-   --          formatting = {
-   --             format = lspkind.cmp_format({
-   --                with_text = false,
-   --                maxwidth = 120,
-   --                mode = "symbol",
-   --                menu = {
-   --                   buffer = "BUF",
-   --                   rg = "RG",
-   --                   nvim_lsp = "LSP",
-   --                   path = "PATH",
-   --                   luasnip = "SNIP",
-   --                   calc = "CALC",
-   --                   spell = "SPELL",
-   --                },
-   --             }),
-   --          },
-   --          -- snippet = {
-   --          --    expand = function(args)
-   --          --       require("luasnip").lsp_expand(args.body)
-   --          --    end,
-   --          -- },
-   --          mapping = {
-   --             ["<C-d>"] = cmp.mapping.scroll_docs(4),
-   --             ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-   --             ["<C-Space>"] = cmp.mapping.complete(),
-   --             ["<C-e>"] = cmp.mapping.close(),
-   --             ["<CR>"] = cmp.mapping.confirm({
-   --                behavior = cmp.ConfirmBehavior.Replace,
-   --                select = false,
-   --             }),
-   --             ["<C-j>"] = cmp.mapping(function(fallback)
-   --                if cmp.visible() then
-   --                   cmp.select_next_item()
-   --                else
-   --                   fallback()
-   --                end
-   --             end, { "i", "s" }),
-   --             ["<C-k>"] = cmp.mapping(function()
-   --                if cmp.visible() then
-   --                   cmp.select_prev_item()
-   --                end
-   --             end, { "i", "s" }),
-   --          },
-   --          sources = {
-   --             { name = "nvim_lua" },
-   --             { name = "path" },
-   --             { name = "nvim_lsp" },
-   --             { name = "nvim_lsp_signature_help" },
-   --             { name = "git" },
-   --             { name = "buffer", keyword_length = 5 },
-   --             { name = "luasnip" },
-   --             { name = "rg", keyword_length = 5 },
-   --          },
-   --       })
-   --
-   --       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-   --       ---@diagnostic disable-next-line: missing-fields
-   --       cmp.setup.cmdline("/", {
-   --          mapping = cmp.mapping.preset.cmdline(),
-   --          sources = cmp.config.sources({
-   --             { name = "nvim_lsp_document_symbol" },
-   --          }, {
-   --             { name = "buffer" },
-   --          }),
-   --       })
-   --
-   --       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-   --       ---@diagnostic disable-next-line: missing-fields
-   --       cmp.setup.cmdline(":", {
-   --          mapping = cmp.mapping.preset.cmdline(),
-   --          sources = cmp.config.sources({
-   --             { name = "path" },
-   --          }, {
-   --             { name = "cmdline" },
-   --          }),
-   --       })
-   --    end,
-   -- },
 }
