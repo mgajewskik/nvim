f942b96f65833605e78cfc42a99dae0b6f3c6618
mgajewskik <mgajewskik@gmail.com>
2026-04-21 18:34:54 +0200

    bring back treesitter and fzk

--- init.lua
+++ init.lua
@@ -366,10 +366,12 @@ augroup("AutoBashType", function(g)
    vim.api.nvim_create_autocmd("BufReadPost", {
       group = g,
       pattern = "*",
-      callback = function()
-         if vim.bo.filetype == "" then
-            vim.opt_local.filetype = "bash"
-         end
+      callback = function(event)
+         vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(event.buf) and vim.bo[event.buf].filetype == "" then
+               vim.bo[event.buf].filetype = "bash"
+            end
+         end)
       end,
    })
 end)
@@ -785,7 +787,7 @@ local plugins = {
          { "<leader>rf", ":FzfLua files cwd=$HOME/<CR>", { noremap = true } },
          { "<leader>sn", ":FzfLua files cwd=$HOME/.config/nvim/<CR>", { noremap = true } },
          { "<leader>si", ":FzfLua files cwd=$HOME/.config/<CR>", { noremap = true } },
-         { "<leader>sw", ":FzfLua files cwd=$WORKSPACE/notes/<CR>", { noremap = true } },
+         { "<leader>sw", ":FzfLua files cwd=$HOME/notes/<CR>", { noremap = true } },
          { "<leader>f", ":FzfLua git_files<CR>", { noremap = true } },
          { "<leader>`", ":FzfLua buffers<CR>", { noremap = true } },
          { "<leader><leader>", ":FzfLua buffers<CR>", { noremap = true } },
@@ -850,6 +852,7 @@ local plugins = {
       end,
       config = function(_, opts)
          require("fzf-lua").setup(opts or {})
+         local notes_cwd = vim.fn.expand("$HOME/notes")
 
          vim.keymap.set("n", "<leader>ws", ":FzfLua grep_cword<CR>", { noremap = true })
          vim.keymap.set("n", "<C-r>", function()
@@ -858,6 +861,12 @@ local plugins = {
          vim.keymap.set("n", "<leader>rr", function()
             utils.home_fzf()
          end, { noremap = true })
+         vim.keymap.set("n", "<leader>zf", function()
+            require("fzf-lua").files({ cwd = notes_cwd })
+         end, { noremap = true, silent = true, desc = "Notes files" })
+         vim.keymap.set("n", "<leader>zs", function()
+            require("fzf-lua").live_grep({ cwd = notes_cwd })
+         end, { noremap = true, silent = true, desc = "Notes grep" })
       end,
    },
    {
@@ -871,11 +880,173 @@ local plugins = {
       },
    },
    {
+      "nvim-treesitter/nvim-treesitter",
+      branch = "main",
+      version = false,
+      lazy = false,
+      build = function()
+         local ok, ts = pcall(require, "nvim-treesitter")
+         if ok and ts.update then
+            ts.update()
+         end
+      end,
+      opts = {
+         highlight = { enable = true },
+         indent = { enable = true },
+         folds = { enable = false },
+         ensure_installed = {
+            "bash",
+            "css",
+            "cmake",
+            "make",
+            "dockerfile",
+            "go",
+            "gomod",
+            "gowork",
+            "gosum",
+            "gotmpl",
+            "proto",
+            "hcl",
+            "terraform",
+            "html",
+            "javascript",
+            "typescript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "toml",
+            "yaml",
+            "rego",
+            "comment",
+            "diff",
+            "gitcommit",
+            "gitignore",
+            "regex",
+            "sql",
+            "vim",
+            "vimdoc",
+         },
+      },
+      config = function(_, opts)
+         local ts = require("nvim-treesitter")
+
+         if not ts.get_installed then
+            vim.notify("nvim-treesitter: run :TSUpdate and restart", vim.log.levels.ERROR)
+            return
+         end
+
+         ts.setup(opts)
+
+         local installed = ts.get_installed()
+         local to_install = vim.tbl_filter(function(lang)
+            return not vim.tbl_contains(installed, lang)
+         end, opts.ensure_installed or {})
+
+         if #to_install > 0 then
+            ts.install(to_install)
+         end
+
+         vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("treesitter_setup", { clear = true }),
+            callback = function(ev)
+               local buf = ev.buf
+               local ft = ev.match
+               local lang = vim.treesitter.language.get_lang(ft) or ft
+
+               local has_parser = pcall(vim.treesitter.language.inspect, lang)
+               if not has_parser then
+                  return
+               end
+
+               if opts.highlight and opts.highlight.enable then
+                  pcall(vim.treesitter.start, buf)
+               end
+
+               if opts.indent and opts.indent.enable then
+                  vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+               end
+
+               if opts.folds and opts.folds.enable then
+                  vim.wo[0][0].foldmethod = "expr"
+                  vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+               end
+            end,
+         })
+
+         vim.treesitter.language.register("markdown", "octo")
+      end,
+   },
+   {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+      event = "VeryLazy",
+      opts = {
+         select = { lookahead = true },
+         move = { set_jumps = true },
+      },
+      config = function(_, opts)
+         local ts_textobjects = require("nvim-treesitter-textobjects")
+         if not ts_textobjects.setup then
+            vim.notify("nvim-treesitter-textobjects: update required", vim.log.levels.ERROR)
+            return
+         end
+         ts_textobjects.setup(opts)
+
+         vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("treesitter_textobjects", { clear = true }),
+            callback = function(ev)
+               local buf = ev.buf
+               local ft = ev.match
+               local lang = vim.treesitter.language.get_lang(ft) or ft
+               local has_parser = pcall(vim.treesitter.language.inspect, lang)
+               if not has_parser then
+                  return
+               end
+
+               local select_mod = require("nvim-treesitter-textobjects.select")
+               local move_mod = require("nvim-treesitter-textobjects.move")
+
+               local select_maps = {
+                  ["af"] = "@function.outer",
+                  ["if"] = "@function.inner",
+                  ["ac"] = "@class.outer",
+                  ["ic"] = "@class.inner",
+                  ["al"] = "@loop.outer",
+                  ["il"] = "@loop.inner",
+                  ["ab"] = "@block.outer",
+                  ["ib"] = "@block.inner",
+                  ["ar"] = "@parameter.outer",
+                  ["ir"] = "@parameter.inner",
+               }
+               for key, query in pairs(select_maps) do
+                  vim.keymap.set({ "x", "o" }, key, function()
+                     select_mod.select_textobject(query, "textobjects")
+                  end, { buffer = buf, silent = true })
+               end
+
+               local move_maps = {
+                  ["]]"] = { "goto_next_start", "@function.outer" },
+                  ["[["] = { "goto_previous_start", "@function.outer" },
+                  ["]m"] = { "goto_next_start", "@class.outer" },
+                  ["[m"] = { "goto_previous_start", "@class.outer" },
+               }
+               for key, cfg in pairs(move_maps) do
+                  vim.keymap.set({ "n", "x", "o" }, key, function()
+                     move_mod[cfg[1]](cfg[2], "textobjects")
+                  end, { buffer = buf, silent = true })
+               end
+            end,
+         })
+      end,
+   },
+   {
       "ray-x/go.nvim",
       dependencies = {
          "ray-x/guihua.lua",
          "neovim/nvim-lspconfig",
-         -- "nvim-treesitter/nvim-treesitter",
+         "nvim-treesitter/nvim-treesitter",
       },
       lazy = true,
       ft = { "go", "gomod" },
@@ -977,6 +1148,7 @@ local plugins = {
             yaml = { "yamllint" },
             go = { "revive" },
             lua = { "luacheck" },
+            sh = { "shellcheck" },
             terraform = { "tfsec" },
             dockerfile = { "hadolint" },
             json = { "jsonlint" },
